caa6a8b3c57c1fafe5a29f7508504995748156ed
Myles "Mellurboo" Wilson <mellurblur@gmail.com>
2025-06-04 00:52:16 +0100

    Added no build file and compatible colours
    the TTY in minos doesn't support bold colours so I just made them not bold for now to save me time changing the colour code over and then re adding it later.

--- include/utils/terminal.h
+++ include/utils/terminal.h
@@ -3,57 +3,23 @@
 
 #define CLEAR_SCREEN() printf("\033[2J\033[H")
 
-/*
-    GCC Colour Escape Codes (More Portable)
-*/
-#if defined(__GNUC__) && !defined(__MINGW32__)
-#define BLACK   "\e[0;30m"
-#define RED     "\e[0;31m"
-#define GREEN   "\e[0;32m"
-#define YELLOW  "\e[0;33m"
-#define BLUE    "\e[0;34m"
-#define MAGENTA "\e[0;35m"
-#define CYAN    "\e[0;36m"
-#define WHITE   "\e[0;37m"
+#define BLACK   "\033[30m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[94m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
 
-#define BBLACK   "\e[1;30m"
-#define BRED     "\e[1;31m"
-#define BGREEN   "\e[1;32m"
-#define BYELLOW  "\e[1;33m"
-#define BBLUE    "\e[1;34m"
-#define BMAGENTA "\e[1;35m"
-#define BCYAN    "\e[1;36m"
-#define BWHITE   "\e[1;37m"
-#define RESET    "\e[0m"
-#endif
-
-/*
-    MINGW Colour Escape codes (For Windows)
-    these are more strict, using the GNUC based escape
-    codes works, however it leaves residual characters because
-    ofc it does, to clarify this only happens in the legacy CMD not
-    the new Microsoft Terminal.
-*/
-#if defined(__MINGW32__)
-#define BLACK   "\x1b[0;30m"
-#define RED     "\x1b[0;31m"
-#define GREEN   "\x1b[0;32m"
-#define YELLOW  "\x1b[0;33m"
-#define BLUE    "\x1b[0;34m"
-#define MAGENTA "\x1b[0;35m"
-#define CYAN    "\x1b[0;36m"
-#define WHITE   "\x1b[0;37m"
-
-#define BBLACK   "\x1b[1;30m"
-#define BRED     "\x1b[1;31m"
-#define BGREEN   "\x1b[1;32m"
-#define BYELLOW  "\x1b[1;33m"
-#define BBLUE    "\x1b[1;34m"
-#define BMAGENTA "\x1b[1;35m"
-#define BCYAN    "\x1b[1;36m"
-#define BWHITE   "\x1b[1;37m"
-
-#define RESET    "\x1b[0m"
-#endif
+#define BBLACK   "\033[30m"
+#define BRED     "\033[31m"
+#define BGREEN   "\033[32m"
+#define BYELLOW  "\033[33m"
+#define BBLUE    "\033[34m"
+#define BMAGENTA "\033[35m"
+#define BCYAN    "\033[36m"
+#define BWHITE   "\033[37m"
+#define RESET    "\033[0m"
 
 #endif
---
+++ nob.c
@@ -0,0 +1,16 @@
+#define NOB_STRIP_PREFIX
+#define NOB_IMPLEMENTATION
+#include "../nob.h"
+
+int main(int argc, char** argv) {
+    NOB_GO_REBUILD_URSELF(argc, argv); // <- That way nob automatically rebuilds itself when changed.
+    Cmd cmd = { 0 };
+    cmd_append(&cmd, "make");
+    if(!cmd_run_sync_and_reset(&cmd)) return 1;
+    char* minos_root = getenv("MINOSROOT");
+    if(!minos_root) {
+        nob_log(NOB_ERROR, "Missing $MINOSROOT");
+        return 1;
+    }
+    if(!copy_file("bin/specseek_64", temp_sprintf("%s/initrd/user/specseek", minos_root))) return 1;
+}
