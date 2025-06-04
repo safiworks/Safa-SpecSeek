12dce7138232bb16f208e34f493c02c75fa078b2
Myles "Mellurboo" Wilson <mellurblur@gmail.com>
2025-06-04 02:21:45 +0100

    made nob.c auto generate

--- install_specseek.sh
+++ install_specseek.sh
@@ -16,8 +16,34 @@ fi
 
 if grep -q "\"specseek\"" "$NOB_PROJECTS"; then
     echo "specseek was already in user/nob.c projects array?"
-    exit 0
 else
     sed -i "/projects\[\].*{/,/};/ s/};/    \"specseek\",\n};/" "$NOB_PROJECTS"
     echo "added specseek to user/nob.c projects array"
 fi
+
+    echo "Writing nob.c to $TARGET_DIR/nob.c"
+    cat > "$TARGET_DIR/nob.c" <<'EOF'
+#define NOB_STRIP_PREFIX
+#define NOB_IMPLEMENTATION
+#include "../nob.h"
+
+int main(int argc, char** argv) {
+    NOB_GO_REBUILD_URSELF(argc, argv); // <- That way nob automatically rebuilds itself when changed.
+    Cmd cmd = { 0 };
+    cmd_append(&cmd, "make");
+    if(!cmd_run_sync_and_reset(&cmd)) return 1;
+    char* minos_root = getenv("MINOSROOT");
+    if(!minos_root) {
+        nob_log(NOB_ERROR, "Missing $MINOSROOT");
+        return 1;
+    }
+    if(!copy_file("bin/minos/64/specseek_minos_64", temp_sprintf("%s/initrd/user/specseek", minos_root))) return 1;
+}
+EOF
+
+    # Confirm the file was written
+    if [ -f "$TARGET_DIR/nob.c" ]; then
+        ls -l "$TARGET_DIR/nob.c"
+    else
+        echo "Failed to generate nob.c"
+    fi
